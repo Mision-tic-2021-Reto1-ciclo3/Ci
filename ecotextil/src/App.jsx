ed37492b230a3c28e46abc9ec9ea0b17b3d13988
Ricardo Pineda <rikaldero@hotmail.com>
2021-10-06 20:47:45 -0500

    Enrutamiento_de login

--- ecotextil/src/App.jsx
+++ ecotextil/src/App.jsx
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Switch>
+        <Route>
+          <Login/>
+        </Route>
+      </Switch>
+    
 
-      
-    </div>
+    </Router>
   );
 }
 
---
+++ ecotextil/src/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <div>
+            Pantalla de inicio luego del Logeo
+        </div>
+    )
+}
+
+export default Index
